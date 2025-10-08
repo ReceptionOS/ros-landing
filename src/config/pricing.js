1e3868d3797252b7c762700b229fc755caf976e0
apoloniadev <dev@apoloniadental.pl>
2025-10-08 12:55:08 +0200

    Update pricing.js

--- src/config/pricing.js
+++ src/config/pricing.js
@@ -2,4 +2,4 @@ export const starterPacketPrice = 1300
 export const optimumPacketPrice = 2300
 export const elitePacketPrice = 3300
 export const incrementPacket = 200
-export const analysisPrice = 2900
+export const analysisPrice = 190
