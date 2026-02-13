7b270cd0aad6d83f81668503965da8c9341a0957
maciejmisztalski <maciej.misztalski99@gmail.com>
2026-02-13 14:05:54 +0100

    feat: base structure

---
+++ src/views/offer/components/FeaturedSection/FeaturedSection.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { FeaturedSectionComponent } from "./styled.components"
+
+const FeaturedSection = ({ t }) => {
+  return (
+    <FeaturedSectionComponent>
+      
+    </FeaturedSectionComponent>
+  )
+}
+export default FeaturedSection
