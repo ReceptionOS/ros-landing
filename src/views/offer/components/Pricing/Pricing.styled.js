85254bef336565b2f2f0582aef7f3cd1378f9381
Robert Babiński <robert.babinski95@gmail.com>
2026-02-18 15:37:08 +0100

    struct improvements

---
+++ src/views/offer/components/Pricing/Pricing.styled.js
@@ -0,0 +1,70 @@
+import styled from "styled-components"
+
+export const PricingWrapper = styled.div`
+  color: white;
+`
+
+export const PricingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const PricingHeader = styled.div``
+
+export const PricingInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const PricingTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const PTopPricing = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const PTopFeatures = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const PricingMiddle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const PMiddleCalc = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const PMiddleNumbers = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
+
+export const PricingBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`
